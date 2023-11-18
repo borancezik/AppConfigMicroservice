08fe4c73075db17a78f640a6e39775e1f2941cce
Boran Cezik <borancezik@gmail.com>
2023-11-18 03:39:42 +0300

    editted ApiResponse model

--- AppConfigMicroservice/Domain/ApiResponse.cs
+++ AppConfigMicroservice/Domain/ApiResponse.cs
@@ -4,7 +4,7 @@ namespace AppConfigMicroservice.Domain
 {
     public class ApiResponse
     {
-        public IModel Data { get; set; }
+        public TResponse Data { get; set; }
         public bool IsSuccess { get; set; } = false;
 
         public string? ErrorCode { get; set; }
